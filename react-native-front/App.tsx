47ff07678d195bebe6f1e316c99e9bda665eb87d
Matt Clark <clarkm02@gmail.com>
2024-05-17 12:50:31 +0200

    feat: navigation in react-native

--- react-native-front/App.tsx
+++ react-native-front/App.tsx
@@ -1,12 +1,51 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text } from "react-native";
+import {NavigationContainer} from '@react-navigation/native';
+import {NativeStackScreenProps, createNativeStackNavigator, type NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+
+type RootStackParamList = {
+  Home: undefined, // undefined because you aren't passing any params to the home screen
+  Profile: { name: string };
+};
+
+type HomeScreenNavigationProp = NativeStackScreenProps<
+  RootStackParamList,
+  'Home'
+>;
+
+type ProfileScreenNavigationProp = NativeStackScreenProps<
+  RootStackParamList,
+  'Profile'
+>;
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const HomeScreen = ({navigation}: HomeScreenNavigationProp) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', {name: 'Jane'})
+      }
+    />
+  );
+};
+const ProfileScreen = ({navigation, route}: ProfileScreenNavigationProp) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
